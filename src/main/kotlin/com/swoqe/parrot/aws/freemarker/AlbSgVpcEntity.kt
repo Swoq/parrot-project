3d9cc38f521a4b05efd3736f45996f34d14c9d01
Mykhailo Kratiuk <mkratiuk@atlassian.com>
2023-08-11 14:06:41 +0300

    Update

--- src/main/kotlin/com/swoqe/parrot/aws/freemarker/AlbSgVpcEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.swoqe.parrot.aws.freemarker
-
-data class AlbSgVpcEntity(
-    val docker_image: String,
-    val inner_port: String,
-    val envs: String?
-)
---
+++ src/main/kotlin/com/swoqe/parrot/configuration/service/ValidationService.kt
@@ -0,0 +1,4 @@
+package com.swoqe.parrot.configuration.service
+
+class ValidationService {
+}
